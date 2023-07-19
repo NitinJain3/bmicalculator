fe8bd78c224e3ef2d7d3e9a141f8677672ef82eb
NitinJain3 <nitinpremlatha@gmail.com>
2023-07-19 19:20:22 +0530

    Added function call in main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import bmi_calc
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,3 +17,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bmi_calc = bmi_calc.body_mass_index
